f8b0c4381c60630691adbe3c04abb7a81cd4700d
Swapnil Opash <sra.opash@gmail.com>
2024-01-25 18:31:34 +0530

    fixed

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -106,7 +106,9 @@ exports.notificationMail = async (userData) => {
 
 exports.channelNotificationEmail = async (userData) => {
   let name = userData?.Username;
-  let msg = `you are assigned in HT channel by MasterAdmin`;
+  let msg = `You have been assign in ChristianTeam channel by the ChristianTeam Admin.
+  To access your channel, log into your ChristianTeam account,click on the
+  ChristianTeam icon at the top of the page,then click on My Channel.`;
   let redirectUrl = `${environment.FRONTEND_URL}`;
 
   const mailObj = {
@@ -131,9 +133,9 @@ exports.communityApproveEmail = async (profileId, isApprove) => {
       userData[0]?.FirstName + " " + userData[0]?.LastName;
     let msg = "";
     if (isApprove === "Y") {
-      msg = `Your Church has been approved by Master Admin.`;
+      msg = `ChristianTeam has approved your Church account.`;
     } else {
-      msg = `Your Church has been unapproved by Master Admin.`;
+      msg = `ChristianTeam has unapproved your Church account.`;
     }
     let redirectUrl = `${environment.FRONTEND_URL}`;
     const mailObj = {
